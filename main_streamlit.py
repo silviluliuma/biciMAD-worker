824d1a65091a6d32772af8ba0e86563a3c8eac3d
Silvia Martinez Valero <valeromsilvia@gmail.com>
2024-01-25 18:24:39 +0100

    .

--- main_streamlit.py
+++ main_streamlit.py
@@ -149,18 +149,20 @@ def get_route_map(stations_real_time, number_district_sidebar, s_sidebar, van_si
 
     return m   
 
-
 stations_real_time = get_stations()
-st.sidebar.title("BiciMAD-worker")
-st.title("Esta es la ruta recomendada para su distrito:")
-number_district_sidebar = st.sidebar.selectbox("¿A qué distrito se le ha asignado hoy?", ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21"], index=0)
-s_sidebar = st.sidebar.text_input('Si esta es su ruta inicial, introduzca "Yes". En caso contrario, introduzca sus coordenadas entre corchetes ([])', 'Yes')
-van_sidebar = st.sidebar.selectbox("¿Su furgoneta está vacía ('Empty') o llena ('Full')?", ["Empty", "Full"], index=0)
-route_map = get_route_map(stations_real_time, number_district_sidebar, s_sidebar, van_sidebar)
-st_data = folium_static(route_map)
-st.text("""Instrucciones de reparto BiciMAD-worker: 
-1. Por favor, recoja las bicicletas en las estaciones naranjas.
-2. Descárguelas en las estaciones verdes.
-3. Si todavía queda tiempo en su jornada laboral, reinicie la aplicación e introduzca sus nuevas coordenadas.
-4. Conduzca con cuidado y que tenga un buen turno.""")
-st.success('Map successfully created!')
+
+if __name__ == "__main__":
+    print(get_stations())
+    st.sidebar.title("BiciMAD-worker")
+    st.title("Esta es la ruta recomendada para su distrito:")
+    number_district_sidebar = st.sidebar.selectbox("¿A qué distrito se le ha asignado hoy?", ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21"], index=0)
+    s_sidebar = st.sidebar.text_input('Si esta es su ruta inicial, introduzca "Yes". En caso contrario, introduzca sus coordenadas entre corchetes ([])', 'Yes')
+    van_sidebar = st.sidebar.selectbox("¿Su furgoneta está vacía ('Empty') o llena ('Full')?", ["Empty", "Full"], index=0)
+    route_map = get_route_map(stations_real_time, number_district_sidebar, s_sidebar, van_sidebar)
+    st_data = folium_static(route_map)
+    st.text("""Instrucciones de reparto BiciMAD-worker: 
+    1. Por favor, recoja las bicicletas en las estaciones naranjas.
+    2. Descárguelas en las estaciones verdes.
+    3. Si todavía queda tiempo en su jornada laboral, reinicie la aplicación e introduzca sus nuevas coordenadas.
+    4. Conduzca con cuidado y que tenga un buen turno.""")
+    st.success('Map successfully created!')
