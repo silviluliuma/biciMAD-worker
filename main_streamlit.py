fec55e90876dffb7ffd64d9881df9fa5461b9741
Silvia Martinez Valero <valeromsilvia@gmail.com>
2024-01-31 13:05:00 +0100

    added df

--- main_streamlit.py
+++ main_streamlit.py
@@ -148,12 +148,12 @@ def get_route_map(stations_real_time, number_district_sidebar, s_sidebar, van_si
 stations_real_time = get_stations()
 
 if __name__ == "__main__":
-    st.write(stations_real_time)
     st.sidebar.title("BiciMAD-worker")
     st.title("Esta es la ruta recomendada para su distrito:")
     number_district_sidebar = st.sidebar.selectbox("¿A qué distrito se le ha asignado hoy?", ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21"], index=0)
     s_sidebar = st.sidebar.text_input('Si esta es su ruta inicial, introduzca "Yes". En caso contrario, introduzca sus coordenadas entre corchetes ([])', 'Yes')
     van_sidebar = st.sidebar.selectbox("¿Su furgoneta está vacía ('Empty') o llena ('Full')?", ["Empty", "Full"], index=0)
+    st.write(get_district(stations_real_time, number_district_sidebar))
     route_map = get_route_map(stations_real_time, number_district_sidebar, s_sidebar, van_sidebar)
     st_data = folium_static(route_map)
     st.text("""Instrucciones de reparto BiciMAD-worker: 
