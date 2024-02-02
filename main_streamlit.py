e24b8d78ecddb521dd662eb8b48f17a8b2bbbcfc
Silvia Martinez Valero <valeromsilvia@gmail.com>
2024-02-02 14:46:30 +0100

    .

--- main_streamlit.py
+++ main_streamlit.py
@@ -149,7 +149,8 @@ try:
     stations_real_time = get_stations()
 except:
     stations_real_time = pd.read_csv("bicimad_stations_backup.csv")
-    stations_real_time[["longitude", "latitude"]] = stations_real_time["geometry.coordinates"].apply(lambda x: pd.Series(x["coordinates"]))
+    stations_real_time[["longitude", "latitude"]] = stations_real_time["geometry"].apply(lambda x: pd.Series(x["coordinates"]))
+    stations_real_time = stations_real_time.drop(["geofence", "activate", "geometry", "integrator", "reservations_count", "no_available", "tipo_estacionPBSC", "virtualDelete", "virtual_bikes", "virtual_bikes_num", "code_suburb", "geofenced_capacity", "bikesGo"], axis=1)
     stations_real_time['coordinates'] = list(zip(stations_real_time['longitude'], stations_real_time['latitude']))
 
 stations_streamlit = stations_real_time[(stations_real_time["light"] == 1) | (stations_real_time["light"] == 0)]
