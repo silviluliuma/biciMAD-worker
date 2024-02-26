a36839706b789798f63fc6bc88a18b78bd7dac80
Silvia Martinez Valero <valeromsilvia@gmail.com>
2024-02-26 19:32:43 +0100

    .

--- Ruta.py
+++ Ruta.py
@@ -73,7 +73,7 @@ def number_DivIcon(color,number): #Crea iconos numerados para las paradas que ti
 loc = get_geolocation() #Con un componente de streamlit, detecta la ubicación actual del usuario
 user_latitude = loc.get("coords").get("latitude")
 user_longitude = loc.get("coords").get("longitude")
-st.write(loc)
+st.write(type(loc))
 
 def get_route_map_google(stations_real_time, number_district_sidebar, van_sidebar): #Hace display de la ruta del trabajador tanto en google maps como en un mapa folium
     client = ors.Client(key=st.secrets["openroute_api_key"])
