5d003d35531d4287dab40a5cae66a315a0b04bb4
Silvia Martinez Valero <valeromsilvia@gmail.com>
2024-02-26 19:08:27 +0100

    .

--- Ruta.py
+++ Ruta.py
@@ -12,7 +12,10 @@ from folium.features import DivIcon
 from streamlit_js_eval import get_geolocation
 import matplotlib.pyplot as plt
 import seaborn as sns
-import time
+import warnings
+
+warnings.filterwarnings("ignore")
+
 #Funciones
 
 def get_token(): #Coger el token de la api de bicimad
@@ -72,7 +75,6 @@ def number_DivIcon(color,number): #Crea iconos numerados para las paradas que ti
 
 
 loc = get_geolocation() #Con un componente de streamlit, detecta la ubicación actual del usuario
-time.sleep(3)
 user_latitude = loc.get("coords").get("latitude")
 user_longitude = loc.get("coords").get("longitude")
 
