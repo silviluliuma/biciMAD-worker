8d816fb5dc05a1094434a8b39b4cb175e5a47f1a
Silvia Martinez Valero <valeromsilvia@gmail.com>
2024-03-02 12:55:02 +0100

    .

--- Ruta.py
+++ Ruta.py
@@ -100,7 +100,7 @@ def get_route_map_google(client, stations_real_time, number_district_sidebar, va
     coords_list = [current_coords]
     stop_counter = 1 
     
-    for i in range(30): #En el distrito 01 hay más estaciones pero el límite de la API es 40 por minuto. Para el resto de las estaciones sobra con 40
+    for i in range(10): #En el distrito 01 hay más estaciones pero el límite de la API es 40 por minuto.
         if van_sidebar == "Empty":
             current_coords = coords_list[-1]
             if not distrito_high.loc[~distrito_high['visited'] & (distrito_high['light'] == 1)].empty:
